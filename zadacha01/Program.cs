b0ffb285833c06cf0ba79579a57f39358d86490a
Минзарский Руслан Радосавович <minzarskiy@olimproekt.ru>
2022-06-29 18:42:40 +0300

    Написал программу по нахождению максимального и минимального значения из 2 чисел

---
+++ zadacha01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine( "Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: " );
+int b = int.Parse(Console.ReadLine()!);
+if (a > b)
+{
+    Console.WriteLine($"Максимальное число {a}");
+    Console.WriteLine($"Минимальное число {b}");
+}
+else 
+{
+    Console.WriteLine($"Максимальное число {b}");
+    Console.WriteLine($"Минимальное число {a}");
+}
