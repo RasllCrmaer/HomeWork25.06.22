d4e87efe438d8ef06835df9a47923c670620d967
Минзарский Руслан Радосавович <minzarskiy@olimproekt.ru>
2022-06-30 12:21:58 +0300

    Решили четвертую задачу

---
+++ zadacha02/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.WriteLine( "Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: " );
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: " );
+int c = int.Parse(Console.ReadLine()!);
+
+if (a > b & a > c)
+{
+    Console.WriteLine($"Максимальное число {a}");
+}
+if (b > a & b > c)
+{
+    Console.WriteLine($"Максимальное число {b}");
+}
+if (c > a & c > b)
+{
+    Console.WriteLine($"Максимальное число {c}");
+}
